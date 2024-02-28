7f68733e53f02c52676d76fa61a63c749d30131f
digitsensitive <digit.sensitivee@gmail.com>
2024-02-28 21:43:27 +0100

    add nice output for tiny4cpu assembler

--- assembler/src/main.c
+++ assembler/src/main.c
@@ -3,18 +3,33 @@
 #include "../../shared/tiny4_types.h"
 #include "assembler.h"
 
+/**
+ * @brief Tiny4CPU Assembler
+ *
+ * @param argc
+ * @param argv
+ * @return int
+ */
 int main(int argc, char *argv[]) {
+  printf("\x1B[32;1m");
+  printf("==================\n");
+  printf("Tiny4CPU Assembler\n");
+  printf("==================\n");
+  printf("\x1b[0m");
+
   if (argc != 2) {
-    printf("usage: assembler [file_path_to_t4c_file]\n");
+    fputs("usage: assembler [file_path_to_t4c_file]\n", stderr);
     return -1;
   }
 
   bool assembly_successful = assemble(argv[1]);
 
   if (!assembly_successful) {
-    printf("Assembly failed.\n");
+    fprintf(stderr, "Assembly of file %s failed.\n", argv[1]);
     return -1;
   }
 
+  printf("Assembly of \x1B[32;3m%s\x1b[0m was successful.\n", argv[1]);
+  printf("\n");
   return 0;
 }
